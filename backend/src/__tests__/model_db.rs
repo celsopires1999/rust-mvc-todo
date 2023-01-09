9962e4af6e433a2ff12f1e5fc5156cc07227f5f4
celsopires1999 <celso.pires1999@gmail.com>
2023-01-09 00:25:21 +0000

    feat: Add App Db init for Dev

--- backend/src/__tests__/model_db.rs
+++ backend/src/__tests__/model_db.rs
@@ -2,7 +2,12 @@ use super::init_db;
 
 #[tokio::test]
 async fn model_db_init_db() -> Result<(), Box<dyn std::error::Error>> {
+    // ACTION
     let db = init_db().await?;
+
+    // CHECK = ASSERT
+    let result = sqlx::query("SELECT * from todo").fetch_all(&db).await?;
+    assert_eq!(2, result.len(), "number of seed todos");
 
     Ok(())
 }
