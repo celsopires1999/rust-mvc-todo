d72af8918d05d71b28a2119be14425c5a5167fe1
celsopires1999 <celso.pires1999@gmail.com>
2023-01-10 00:50:20 +0000

    feat: Add sqlb on create and list methods

--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,3 +1,6 @@
+use sqlb::HasFields;
+use sqlx::Executor;
+
 use super::db::Db;
 use crate::model;
 
@@ -10,9 +13,8 @@ pub struct Todo {
     pub status: TodoStatus,
 }
 
-#[derive(Default, Clone)]
+#[derive(sqlb::Fields, Default, Clone)]
 pub struct TodoPatch {
-    pub cid: Option<i64>,
     pub title: Option<String>,
     pub status: Option<TodoStatus>,
 }
@@ -24,29 +26,32 @@ pub enum TodoStatus {
     Open,
     Close,
 }
+sqlb::bindable!(TodoStatus);
 // endregion:   Todo Types
 
 // region:       TodoMac
 pub struct TodoMac;
 impl TodoMac {
     pub async fn create(db: &Db, data: TodoPatch) -> Result<Todo, model::Error> {
-        let sql = "INSERT INTO todo (cid, title) VALUES ($1, $2) returning id, cid, title, status";
-        let query = sqlx::query_as::<_, Todo>(&sql)
-            .bind(123 as i64)
-            .bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+        let mut fields = data.fields();
+        fields.push(("cid", 123).into());
+        let sb = sqlb::insert()
+            .table("todo")
+            .data(fields)
+            .returning(&["id", "cid", "title", "status"]);
 
-        let todo = query.fetch_one(db).await?;
+        let todo = sb.fetch_one(db).await?;
 
         Ok(todo)
     }
 
     pub async fn list(db: &Db) -> Result<Vec<Todo>, model::Error> {
-        let sql = "SELECT id, cid, title, status FROM todo ORDER BY id DESC";
+        let sb = sqlb::select()
+            .table("todo")
+            .columns(&["id", "cid", "title", "status"])
+            .order_by("!id");
 
-        // build the sqlx-query
-        let query = sqlx::query_as(&sql);
-        // execute the query
-        let todos = query.fetch_all(db).await?;
+        let todos = sb.fetch_all(db).await?;
 
         Ok(todos)
     }
